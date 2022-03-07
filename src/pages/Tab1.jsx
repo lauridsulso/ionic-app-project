9b85b1f99798610ae997bdfe41dcb1172f880b9d
lauridsulso <laurids_ulso@hotmail.com>
2022-03-07 10:25:03 +0100

    Template cleanup
    delete unwanted stuff and imports

--- src/pages/Tab1.jsx
+++ src/pages/Tab1.jsx
@@ -1,8 +1,12 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 
-const Tab1: React.FC = () => {
+export const Tab1 = () => {
   return (
     <IonPage>
       <IonHeader>
@@ -16,10 +20,7 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
   );
 };
-
-export default Tab1;
