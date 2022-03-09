84264509de67fd2fc884a21bc6761eb26a23dc37
emilbergs <emilberg@outlook.dk>
2022-03-09 15:57:12 +0100

    🔥 Add firebase and read data from realtime db

---
+++ src/firebase-config.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8haO6WQ2tsKOYvFivc0mB7LIdS4yCHK8",
+  authDomain: "ionic-app-project-8318d.firebaseapp.com",
+  databaseURL: "https://ionic-app-project-8318d-default-rtdb.firebaseio.com",
+  projectId: "ionic-app-project-8318d",
+  storageBucket: "ionic-app-project-8318d.appspot.com",
+  messagingSenderId: "654628867908",
+  appId: "1:654628867908:web:138490293dff4851a79111",
+  measurementId: "G-BZ8BPS81CX",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Create database reference
+const database = getDatabase(app);
+// Reference to posts in Realtime DB
+export const tasksRef = ref(database, "tasks");
+// Reference to users in Realtime DB
+export const usersRef = ref(database, "users");
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,59 @@
 import {
   IonContent,
   IonHeader,
+  IonList,
   IonPage,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
+import { useEffect, useState } from "react";
+import { tasksRef, usersRef } from "../firebase-config";
+import { onValue, get } from "firebase/database";
+import { TaskCardItem } from "../components/TaskCardItem";
 
 export const Home = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    async function getUsers() {
+      const snapshot = await get(usersRef);
+      const usersArray = [];
+      snapshot.forEach((taskSnapshot) => {
+        const id = taskSnapshot.key;
+        const data = taskSnapshot.val();
+        const user = {
+          id,
+          ...data,
+        };
+        usersArray.push(user);
+      });
+
+      return usersArray;
+    }
+
+    async function listenOnChange() {
+      const users = await getUsers();
+      onValue(tasksRef, async (snapshot) => {
+        const tasksArray = [];
+        snapshot.forEach((taskSnapshot) => {
+          const id = taskSnapshot.key;
+          const data = taskSnapshot.val();
+          const task = {
+            id,
+            ...data,
+            user: users.find((user) => user.id === data.uid),
+          };
+          tasksArray.push(task);
+        });
+        setTasks(tasksArray.reverse());
+      });
+    }
+
+    listenOnChange();
+  }, []);
+
   return (
-    <IonPage>
+    <IonPage className="tasks-page">
       <IonHeader>
         <IonToolbar>
           <IonTitle>Home</IonTitle>
@@ -20,6 +65,11 @@ export const Home = () => {
             <IonTitle size="large">Home</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <IonList>
+          {tasks.map((task) => (
+            <TaskCardItem task={task} key={task.id} />
+          ))}
+        </IonList>
       </IonContent>
     </IonPage>
   );
