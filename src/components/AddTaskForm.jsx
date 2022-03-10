67b271eeff43fa527c42497bcf07095e076b2b08
lauridsulso <laurids_ulso@hotmail.com>
2022-03-10 10:00:55 +0100

    update taskform

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,5 +1,18 @@
 import { IonPage } from "@ionic/react";
 
 export const AddTaskForm = () => {
-  return <IonPage></IonPage>;
+  //  -------- USESTATES for adding task data --------
+  // const [title, setTitle] = useState("");
+  // const [description, setDescription] = useState("");
+  // const [image, setImage] = useState("");
+  // const [date, setDate] = useState("");
+  // const [location, setLocation] = useState("");
+
+  return (
+    <IonPage>
+      <form
+      // onSubmit={handleSubmit}
+      ></form>
+    </IonPage>
+  );
 };
